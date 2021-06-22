a3ce151529686559a4c1f90a04c1200a037c1ae6
yongxuanzhang <yongxuanzhang93@gmail.com>
2021-06-21 22:56:46 -0400

    test: integration test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -3,3 +3,17 @@ import os
 from jina import Flow, Document
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+def data_generator(num_docs):
+    for i in range(num_docs):
+        doc = Document(
+            text='it is a good day! the dog sits on the floor.')
+        yield doc
+
+
+def test_use_in_flow():
+    with Flow.load_config('flow.yml') as flow:
+        data = flow.post(on='/encode', inputs=data_generator(5))
+        docs = data[0].docs
+        for doc in docs:
+            assert doc.embedding.shape == (100,)
--- tests/unit/test_encode.py
+++ tests/unit/test_encode.py
@@ -1,18 +1,22 @@
 import os
-
+import pytest
 import numpy as np
 from jina import DocumentArray, Document
-from jinahub.encoder.flair_encoder import FlairTextEncoder
+from jinahub.encoder.flair_text import FlairTextEncoder
 
 directory = os.path.dirname(os.path.realpath(__file__))
 
 docs = DocumentArray([Document(text='it is a good day!, the dog sits on the floor.')])
 
 
-def test_flairtextencoder_encode():
-    encoder = FlairTextEncoder(pooling_strategy='mean')
-    embeddings = {}
+@pytest.fixture()
+def test_text():
+    return 'it is a good day! the dog sits on the floor.'
 
-    docs = DocumentArray([Document(text='it is a good day!, the dog sits on the floor.')])
+
+def test_flairtextencoder_encode(test_text):
+    encoder = FlairTextEncoder(pooling_strategy='mean')
+    docs = DocumentArray([Document(text=test_text)])
     encoder.encode(docs, parameters={})
+
     assert docs[0].embedding.shape == (100,)
